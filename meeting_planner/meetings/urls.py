c4621b6cb97411ccaf599b3bb45dc97540e259e7
Ram <kittuvelagala@gmail.com>
2021-12-19 21:30:50 -0600

    forms backend validationleft.

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
-    path('listofrooms', views.show_room_objects, name="listofrooms")
+    path('listofrooms', views.show_room_objects, name="listofrooms"),
+    path('new', views.new, name="new")
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
+from django.forms import modelform_factory
 # Create your views here.
 
 def detail(request, id):
@@ -9,3 +10,17 @@ def detail(request, id):
 def show_room_objects(request):
     rooms = Room.objects.all()
     return render(request, "meetings/listrooms.html", {"rooms": rooms})
+
+#here meetingform is a class, not a object.
+MeetingForm  = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid(): # see whether the user has given valid data.
+            form.save()
+            return redirect("HomePage")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
+
